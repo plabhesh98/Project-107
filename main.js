ccb671dfa1b9862b89b926735a09353e21f5834d
plabhesh98 <81747071+plabhesh98@users.noreply.github.com>
2021-12-18 19:41:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function start_classification() {
+    navigator.mediaDevices.getUserMedia({audio : true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/PoqqNMeol/model.json", modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error , results) {
+     if (error) {
+        console.error(error);
+     }else {
+         console.log(results);
+     }
+}
